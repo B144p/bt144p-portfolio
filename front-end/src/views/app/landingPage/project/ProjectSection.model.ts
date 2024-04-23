e5ea78c40aaf87bef60ce07e0383b0910eed082d
bt-144p <gug321gug@gmail.com>
2024-04-24 01:18:00 +0700

    feature: project card component

---
+++ front-end/src/views/app/landingPage/project/ProjectSection.model.ts
@@ -0,0 +1,9 @@
+import { HTMLAttributeAnchorTarget } from "react";
+
+export type TProjectList = {
+  name: string;
+  detail: string;
+  tag: string[];
+  path: string;
+  target: HTMLAttributeAnchorTarget;
+};
--- front-end/src/views/app/landingPage/project/ProjectSection.tsx
+++ front-end/src/views/app/landingPage/project/ProjectSection.tsx
@@ -1,62 +1,84 @@
 import { Col, Row } from "antd";
 import { FC } from "react";
 import { styled } from "styled-components";
-import { colors } from "../../../../utils/colors";
+import ProjectCard from "../../../../components/ProjectCard";
+import { openNewTabURL } from "../../../../utils/functions";
+import { TProjectList } from "./ProjectSection.model";
 
 type Props = {};
 
 const ProjectSectionStyled = styled.div``;
 
 const ProjectSection: FC<Props> = () => {
-  const projectList = [
-    { name: "Playground-React", path: "https://playground-react-ts.vercel.app/", target: "_blank" },
-    { name: "React-Workshop", path: "#about" },
-    { name: "Poke-Dex", path: "#about" },
-    { name: "Portfolio V.1", path: "#about" },
+  const projectList: TProjectList[] = [
+    {
+      name: "Playground-React",
+      detail: "React-TS",
+      tag: ["React", "Typescript", "Sprites-sheet", "SCSS"],
+      path: "https://playground-react-ts.vercel.app/",
+      target: "_blank",
+    },
+    {
+      name: "React-Workshop",
+      detail: "React-JS Lab",
+      tag: ["Javascript"],
+      path: "#about",
+      target: "_self",
+    },
+    {
+      name: "Poke-Dex",
+      detail: "Pokemon dex with UI",
+      tag: ["React", "Typescript", "Antd", "GraphQL", "Poke-API"],
+      path: "#about",
+      target: "_self",
+    },
+    {
+      name: "Portfolio V.1",
+      detail: "Start line of web-developer.",
+      tag: ["React", "MERN-Stack", "Python", "Selenium"],
+      path: "https://test-webport-b144p.netlify.app/",
+      target: "_blank",
+    },
   ];
 
   return (
     <ProjectSectionStyled>
-      <h1>Projects</h1>
-      <Row
-        // justify="center"
-        gutter={[8, 8]}
+      <h1
+        style={{
+          textAlign: "center",
+          fontSize: "2rem",
+        }}
       >
-        {projectList.map((project) => {
-          return (
-            <Col
-              xs={24}
-              sm={12}
-              md={8}
-              key={project.name}
-            >
-              <div
-                style={{
-                  width: "100%",
-                  height: 100,
-                  border: "5px solid white",
-                  borderRadius: "1rem",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  cursor: "pointer",
-                }}
-              >
-                <a
-                  href={project.path}
-                  target={project.target ?? '_self'}
-                  style={{
-                    color: colors.primaryText,
-                    width: '100%',
-                    height: '100%',
-                  }}
-                >
-                  <b>{project.name}</b>
-                </a>
-              </div>
-            </Col>
-          );
-        })}
+        Projects
+      </h1>
+      <Row
+        justify={{
+          xs: "center",
+          sm: "start",
+        }}
+        style={{
+          margin: "2rem",
+          marginTop: 0,
+        }}
+        gutter={[16, 16]}
+      >
+        {projectList.map((project) => (
+          <Col
+            xs={20}
+            sm={8}
+            md={6}
+            key={project.name}
+            onClick={() =>
+              openNewTabURL(project.path, project.target ?? "_self")
+            }
+          >
+            <ProjectCard
+              title={project.name}
+              detail={project.name}
+              tagList={project.tag}
+            />
+          </Col>
+        ))}
       </Row>
     </ProjectSectionStyled>
   );
